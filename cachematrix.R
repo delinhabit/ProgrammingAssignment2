4eaaaa5b94fc4b1114331368897a8abf5bf52cf0
Ion Scerbatiuc <ion@rover.com>
2014-12-20 10:33:53 -0800

    Added comments and finalized the implementation

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+######################################################################
+## Copyright (C) 2014, Ion Scerbatiuc <delinhabit@gmail.com>
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+## 02110-1301, USA
+#####################################################################
 
-## Write a short comment describing this function
+#####################################################################
+## This module provides two utility functions for computing and using
+## the cached version of a matrix.
+#####################################################################
+
 
 makeCacheMatrix <- function(x = matrix()) {
+    # Creates a list of functions that encapsulates a matrix and provides means
+    # for retrieving and storing it's inverse.
+    #
+    # Store the value of the inverse by calling the `setInverse` function.
+    #
+    # The value of the inverse is cached for future calls of `getInverse`,
+    # unless the value of the matrix is changed using the `set` function. You
+    # can retrieve the raw matrix by calling the `get` function.
+    #
+    # Args:
+    #   x: the raw matrix to encapsulate
+    #
+    # Returns:
+    #   A list of functions `set`, `get`, `setInverse` and `getInverse` as
+    #   described above
+    #
     inv <- NULL
     list(
         set = function(y) {
@@ -17,9 +53,19 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
+    # Computes the inverse of the provided "cache matrix" and caches it for
+    # future uses.
+    #
+    # If the inverse of the matrix, was already computed, retrieve it from
+    # the cache.
+    #
+    # Args:
+    #   x: the "cache matrix" list representing the matrix to be solved
+    #
+    # Returns:
+    #   The inverse of the matrix
+    #
     inv <- x$getInverse()
     if (!is.null(inv)) {
         message("Getting cached inverse")
@@ -27,7 +73,7 @@ cacheSolve <- function(x, ...) {
     }
 
     data <- x$get()
-    inv <- solve(data)
+    inv <- solve(data, ...)
     x$setInverse(inv)
     inv
 }
